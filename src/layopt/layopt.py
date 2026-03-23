ab268f5ad8a6fd6f927f2dbe8221aea6b9a37fb4
Neil Shephard <n.shephard@sheffield.ac.uk>
2026-03-23 21:49:54 +0000

    feature: introduce logging via loguru
    Closes #31
    
    Switch to using [loguru](https://loguru.readthedocs.io/en/stable/overview.html) for outputting to terminal when running
    the code.
    
    May wish to consider using `@logger.catch` decorator if there is the possibility of other errors arising and we could
    decide on whether we want to change the log-level for some of the output such that its only shown when debugging and/or
    add more debug logging statements throughout.
    
    Useful tutorial [How to Use Loguru for Simpler Python Logging – Real Python](https://realpython.com/python-loguru/).

--- src/layopt/layopt.py
+++ src/layopt/layopt.py
@@ -27,6 +27,7 @@ import matplotlib.pyplot as plt
 import mosek.fusion as mosek
 import numpy as np
 import numpy.typing as npt
+from loguru import logger
 from scipy import sparse
 
 # from numpy.matlib import repmat
@@ -378,12 +379,15 @@ def make_pattern_loads(
     # ns-rse 2026-03-17 : Return directly as part of tuple
     base_load = all_patterns[0]  # First pattern = all large loads
 
-    print(f"\nPattern loading: {n} load points -> {len(all_patterns)} total patterns")
-    print(f"Base case (all large): {patternodal_coordsescriptions[0]}")
+    logger.info(
+        f"\nPattern loading: {n} load points -> {len(all_patterns)} total patterns"
+    )
+    logger.info(f"Base case (all large): {patternodal_coordsescriptions[0]}")
 
     return all_patterns, base_load, patternodal_coordsescriptions
 
 
+# ns-rse 2026-03-23 : Could this comment perhaps form the extended description for the function?
 # add new pattern load cases primal violation
 # violation criterion: check if min over active j of ||B*q[j] - f[k]*dof|| > tol
 # (essentially checking if Bq-f=0)
@@ -432,13 +436,9 @@ def stop_primal_violation_residual(
             continue  # skip active cases
 
         fk_dof = all_patterns[k] * dof
-        # ns-rse 2026-03-18 : potential list compreshension
-        # residuals = [np.linalg.norm(eq_matrix_b.dot(force) - fk_dof) for force in forces]
-        residuals = []
-        for qj in forces:
-            # store ||B*q[j] - f[k]*dof||
-            residual = np.linalg.norm(eq_matrix_b.dot(qj) - fk_dof)
-            residuals.append(residual)
+        residuals = [
+            np.linalg.norm(eq_matrix_b.dot(force) - fk_dof) for force in forces
+        ]
 
         # find min of residuals over active pattern load cases
         total_violation[k] = min(residuals)
@@ -614,7 +614,7 @@ def stop_primal_violation_pattern(
         # Add most violated (lowest lambda)
         most_violated_id = by_violation[0]
         active_load_cases[most_violated_id] = 1
-        print(
+        logger.info(
             f"  Adding most violated pattern {by_violation[0]}: lambda={load_factors[most_violated_id]:.3f}"
         )
         violations_added_this_iter = [by_violation[0]]
@@ -659,7 +659,7 @@ def stop_primal_violation_pattern(
                             break
                 if distinct:
                     active_load_cases[k] = 1
-                    print(
+                    logger.info(
                         f"  Adding {len(violations_added_this_iter) + 1} distinct pattern {k}: lambda={load_factors[k]:.3f}"
                     )
                     violations_added_this_iter.append(k)
@@ -819,8 +819,8 @@ def trussopt(
         active_load_cases = np.ones(len(all_patterns), dtype=int)
 
     setup_end = time.process_time()
-    print(f"Setup took {setup_end - setup_start!s}")
-    print(
+    logger.info(f"Setup took {setup_end - setup_start!s}")
+    logger.info(
         f"Nodes: {len(nodal_coords)} Members: {len(potential_members)} Total load patterns: {len(all_patterns)}"
     )
 
@@ -851,11 +851,12 @@ def trussopt(
 
         # output
         if isinf(vol):
-            print("Error: infeasible problem detected")
+            logger.error("Infeasible problem detected")
             return [], [], []
         n_active = int(np.sum(active_load_cases))
-        print(
-            f"Itr: {itr}, vol: {vol}, mems: {len(c_n)} active load cases:{n_active}/{len(all_patterns)}"
+        # ns-rse 2026-03-23 : Could this perhaps be debugging?
+        logger.info(
+            f"Iteration: {itr}, vol: {vol}, mems: {len(c_n)} active load cases:{n_active}/{len(all_patterns)}"
         )
         # plot interim solutions (slow)
         # plotTruss(nodal_coords, c_n, a, q, max(a) * 1e-2, "Itr:" + str(itr), extraPlot = activeDamageDef)
@@ -912,10 +913,12 @@ def trussopt(
             break  # Converged
 
     final_vol = vol
-    print(f"Volume: {final_vol}")
+    logger.info(f"Volume: {final_vol}")
     solve_end = time.process_time()
-    print("Solve took " + str(solve_end - setup_end))
-    print(f"Active patterns: {int(np.sum(active_load_cases))}/{len(all_patterns)}")
+    logger.info("Solve took " + str(solve_end - setup_end))
+    logger.info(
+        f"Active patterns: {int(np.sum(active_load_cases))}/{len(all_patterns)}"
+    )
 
     # Plot results
     # plotTruss(nodal_coords, c_n, a, q, max(a)*1e-2, "Final", update=False, allCases=True)
@@ -952,7 +955,7 @@ def trussopt(
             joint_cost,
         )
         if vol > 0:
-            print(
+            logger.info(
                 f"filtered volume {vol} with filter at {100 * multiplier}% gives {len(filer_a)!s} members"
             )
             plot_truss(
@@ -966,7 +969,7 @@ def trussopt(
                 all_cases=False,
             )
 
-    print(f"Plotting took {time.process_time() - solve_end!s}")
+    logger.info(f"Plotting took {time.process_time() - solve_end!s}")
 
     # Save results to CSV
     if save_to_csv:
@@ -1063,7 +1066,7 @@ def save_results_to_csv(
         # Write data
         writer.writerow(results)
 
-    print(f"\nResults saved to {filename}")
+    logger.info(f"\nResults saved to {filename}")
 
 
 # # Example usage:
