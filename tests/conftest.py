2b9dc53d540ca10618c9de3102e7cfde26ceeca8
Andrew Fisher <andrew.fisher@sheffield.ac.uk>
2026-03-25 13:36:26 +0000

    Unit tests for make_pattern_loads plus conftest.py
    -Added pytest fixtures & parametrization. Current total of 7 positive tests and 1 negative test.
    -Negative test `test_make_pattern_loads_zero_load_points_error` currently failing due to empty `load_points` not raising
    an AssertionError.
    -Will need refactoring to match format of ns-rse/42-unit-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,23 @@
 # conftest.py
-import pytest
 import numpy as np
+import pytest
+
 
 @pytest.fixture
 def nodal_coords():
-    return np.array([[x,y] for y in range(2) for x in range(5)])
+    return np.array([[x, y] for y in range(2) for x in range(5)])
+
 
 @pytest.fixture
 def load_large():
     return 3.75
 
+
 @pytest.fixture
 def load_small():
     return 0.204
 
+
 @pytest.fixture
 def load_direction_default():
-    return (0,-1)
+    return (0, -1)
