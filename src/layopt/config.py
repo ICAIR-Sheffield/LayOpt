1b5817c36574000d22f3aea8f395d7c27489f517
Neil Shephard <n.shephard@sheffield.ac.uk>
2026-04-02 13:21:12 +0100

    feature(config): Module for handling configuration
    Adds functions (and tests) for handling configuration.
    
    We want to be able to have a default configuration (already included) but reconcile that with user provided arguments,
    or use a user configuration file if that is provided.
    
    - `reconcile_config_args()` achieves this by checking if the arguments (first parameter) includes `config_file` in which
      case user has specified a configuration file. This is then merged using `merge_mappings()` with the second
      `default_config` which is the second parameter. If there is no `config_file` in the `args` the the `default_config`
    is used (it should have been loaded  first).
    - Finally we use `merge_mapping()` again to reconcile `args` with whatever `config` currently is. as we want to make
      sure command line arguments take precedence.
    - Series of tests are in place to check the functions work as advertised.
    - Added `pytest-loguru` to test dependencies so we can check log messages.

---
+++ src/layopt/config.py
@@ -0,0 +1,91 @@
+"""Module functions for working with configuration files."""
+
+from __future__ import annotations
+
+from argparse import Namespace  # noqa: TC003
+from collections.abc import MutableMapping
+from pprint import pformat
+from typing import Any
+
+import numpy as np
+from loguru import logger
+
+from layopt.io import convert_path, read_yaml
+
+
+def reconcile_config_args(
+    args: Namespace | None, default_config: dict[str, Any]
+) -> dict[str, Any]:
+    """
+    Reconcile command line arguments with the default configuration.
+
+    Command line arguments take precedence over the default configuration. If a partial configuration file is specified
+    (with '-c' or '--config-file') the defaults are over-ridden by these values (internally the configuration
+    dictionary is updated with these values). Any other command line arguments take precedence over both the default and
+    those supplied in a configuration file (again the dictionary is updated).
+
+    The final configuration is validated before processing begins.
+
+    Parameters
+    ----------
+    args : Namespace
+        Command line arguments passed into TopoStats.
+    default_config : dict[str, Any]
+        Dictionary containing the default configuration for the package.
+
+    Returns
+    -------
+    dict[str, Any]
+        The configuration dictionary.
+    """
+    # If we have args check for 'config_file', if present load and merge with default
+    if args is not None and args.config_file is not None:
+        config = read_yaml(str(args.config_file))
+        config = merge_mappings(map1=default_config, map2=config)
+    # If no args we use the default_config
+    else:
+        config = default_config
+    # Override the config with command line arguments
+    if args is not None:
+        config = merge_mappings(map1=config, map2=vars(args))
+    logger.debug(f"\nConfiguration after update : \n{pformat(config, indent=4)}\n")
+    return dict(config)
+
+
+def merge_mappings(map1: MutableMapping, map2: MutableMapping) -> dict[str, Any]:
+    """
+    Merge two mappings (dictionaries), with priority given to the second mapping.
+
+    ``map1`` is updated with values from ``map2``.
+
+    Parameters
+    ----------
+    map1 : MutableMapping
+        First mapping to merge, with secondary priority.
+    map2 : MutableMapping
+        Second mapping to merge, with primary priority.
+
+    Returns
+    -------
+    dict
+        Merged dictionary.
+    """
+    for key, value in map2.items():
+        # Recurse if we have a MutableMapping (e.g. nested dictionary)
+        if isinstance(value, MutableMapping):
+            map1[key] = merge_mappings(map1.get(key, {}), value)
+        # Otherwise update the value
+        else:
+            map1[key] = value
+    if "output_dir" in map1:
+        map1["output_dir"] = convert_path(map1["output_dir"])
+    if "load_direction" in map1:
+        map1["load_direction"] = (
+            map1["load_direction"][0],
+            map1["load_direction"][1],
+        )
+    if "loaded_points" in map1:
+        map1["loaded_points"] = np.asarray(map1["loaded_points"])
+    if "support_points" in map1:
+        map1["support_points"] = np.asarray(map1["support_points"])
+    return dict(map1)
---
+++ tests/test_config.py
@@ -0,0 +1,136 @@
+"""Test the config module functions."""
+
+import argparse
+import logging
+from pathlib import Path
+from pkgutil import get_data
+from typing import Any
+
+import numpy as np
+import numpy.typing as npt
+import pytest
+import yaml
+from schema import SchemaError
+
+from layopt.config import (
+    merge_mappings,
+    reconcile_config_args,
+)
+from layopt.validation import DEFAULT_CONFIG_SCHEMA, validate_config
+
+BASE_DIR = Path.cwd()
+RESOURCES = BASE_DIR / "tests" / "resources"
+
+# Load the 'default_config.yaml' to dictionary.
+default_config = get_data(package="layopt", resource="default_config.yaml")
+DEFAULT_CONFIG = yaml.full_load(default_config)
+
+
+def test_reconcile_config_args_no_config(caplog) -> None:
+    """Test the handling of config file function with no config."""
+    args = argparse.Namespace(
+        config_file=None,
+    )
+    config = reconcile_config_args(args=args, default_config=DEFAULT_CONFIG)
+
+    # Check that the config passes the schema
+    config.pop("config_file")
+    with caplog.at_level(logging.INFO):
+        validate_config(config, schema=DEFAULT_CONFIG_SCHEMA, config_type="default")
+
+    assert "✅ The default configuration is valid." in caplog.text
+
+
+@pytest.mark.parametrize(
+    ("args", "parameter", "value"),
+    [
+        pytest.param(
+            argparse.Namespace(config_file=None, stress_tensile=10.0),
+            "stress_tensile",
+            10.0,
+            id="stress_tensile 10.0",
+        ),
+        pytest.param(
+            argparse.Namespace(config_file=None, loaded_points=[[6, 2]]),
+            "loaded_points",
+            np.asarray([[6, 2]]),
+            id="loaded_points [[6, 2]]",
+        ),
+        pytest.param(
+            argparse.Namespace(config_file=None, member_area_filtering=True),
+            "member_area_filtering",
+            True,
+            id="member_area_filtering True",
+        ),
+    ],
+)
+def test_reconcile_config_args(
+    args: argparse.Namespace,
+    parameter: str,
+    value: float | npt.NDArray | bool,
+) -> None:
+    """Test that passing alternative ``args`` to ``reconcile_config_args()`` updates the configuration."""
+    config: dict[str, Any] = reconcile_config_args(
+        args=args, default_config=DEFAULT_CONFIG
+    )
+    assert config[parameter] == value
+
+
+@pytest.mark.parametrize(
+    ("args"),
+    [
+        pytest.param(
+            argparse.Namespace(config_file=None, stress_tensile=-1.0),
+            id="stress_tensile < 0.0",
+        ),
+        pytest.param(
+            argparse.Namespace(config_file=None, loaded_points=[1, 2]),
+            id="loaded_points 1 dimension",
+        ),
+        pytest.param(
+            argparse.Namespace(config_file=None, member_area_filtering="yes"),
+            id="member_area_filtering str not bool",
+        ),
+    ],
+)
+def test_reconcile_config_args_invalid_args(
+    args: argparse.Namespace,
+) -> None:
+    """Test that providing invalid values for parameters raises Schema errors from ``reconcile_config_args()``."""
+    config: dict[str, Any] = reconcile_config_args(
+        args=args, default_config=DEFAULT_CONFIG
+    )
+    with pytest.raises(SchemaError):
+        validate_config(config, schema=DEFAULT_CONFIG_SCHEMA, config_type="default")
+
+
+@pytest.mark.parametrize(
+    ("dict1", "dict2", "expected_merged_dict"),
+    [
+        pytest.param(
+            {"a": 1, "b": 2},
+            {"c": 3, "d": 4},
+            {"a": 1, "b": 2, "c": 3, "d": 4},
+            id="two dicts, no common keys",
+        ),
+        pytest.param(
+            {"a": 1, "b": 2},
+            {"b": 3, "c": 4},
+            {"a": 1, "b": 3, "c": 4},
+            id="two dicts, one common key, testing priority of second dict",
+        ),
+        # Nested dictionaries
+        pytest.param(
+            {"a": 1, "b": {"c": 2, "d": 3}},
+            {"b": {"c": 4, "e": 5}},
+            {"a": 1, "b": {"c": 4, "d": 3, "e": 5}},
+            id="nested dictionaries, one common key in nested dict, testing priority of second dict",
+        ),
+    ],
+)
+def test_merge_mappings(
+    dict1: dict[str, Any], dict2: dict[str, Any], expected_merged_dict: dict[str, Any]
+) -> None:
+    """Test ``merge_mappings()``."""
+    merged_dict = merge_mappings(dict1, dict2)
+    assert merged_dict == expected_merged_dict
