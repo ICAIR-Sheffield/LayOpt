75426aa56d2ed101a1b79715554b8a43daca3de5
Andrew Fisher <andrew.fisher@sheffield.ac.uk>
2026-09-09 11:16:56 +0100

    Lowered precision for `test_trussopt::spanning_example_elastic_clarabel` which fails on macOS
    -overrides `PRECISION` for that test on all platforms (so only one snapshot per test still)
    -easy to do similar for other `test_trussopt` ids if needed

--- tests/test_layopt.py
+++ tests/test_layopt.py
@@ -298,10 +298,18 @@ def test_trussopt(
     results = (results[0], results[1])
     # note: helen-fairclough 29/7/2026 results[3] (the structure object) is not tested, reconsider when
     # refactoring is more complete
+
+    # fisher568 2026-09-09 override precision for tests where small platform differences
+    # NB done on a test basis not platform basis to main one snapshot per test
+    precision_overrides = {
+        "spanning_example_elastic_clarabel": 5,
+    }
+    test_id = request.node.callspec.id
+    test_precision = precision_overrides.get(test_id, PRECISION)
     assert results == snapshot(
         matcher=path_type(
             types=(float, np.ndarray),
-            replacer=lambda data, _: round_values(data, PRECISION),
+            replacer=lambda data, _: round_values(data, test_precision),
         ),
     )
     assert (
