34faf21ae9bf90d1616f01ab63a9b885bb3a2209
Andrew Fisher <andrew.fisher@sheffield.ac.uk>
2026-09-14 14:03:13 +0100

    Update default number for `cores` from `2` to `4`

--- src/layopt/classes.py
+++ src/layopt/classes.py
@@ -28,7 +28,7 @@ class Parameters:
         title="Path to save the output to, default is './output/'.",
     )
     log_level: str = Field(default="info", title="Log level")
-    cores: int = Field(default=2, title="Cores to run optimisation on in parallel.")
+    cores: int = Field(default=4, title="Cores to run optimisation on in parallel.")
     width: int = Field(default=3, title="Width of structure.", ge=1)
     height: int = Field(default=3, title="Height of structure.", ge=1)
     steps: float = Field(default=1.0, title="Steps to generate nodes", gt=0.0)
@@ -393,7 +393,7 @@ class Structure:
             max_length_initial_ground_structure=self.parameters.max_length_initial_ground_structure,
         )
         self.active_members = self.potential_members[
-            self.potential_members[:, 3] == True
+            self.potential_members[:, 3] == True  # pylint: disable=singleton-comparison
         ]
         self.primal_adaptivity, self.load_case_active = structure.primal_adaptivity(
             primal_method=self.parameters.primal_method,
